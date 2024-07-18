d25f1df99eb8edb526f20b0e5f3f664cff4836d4
grisak4 <grisak4@gmail.com>
2024-07-18 16:09:24 +0700

    сделал обход ngrok-free, настроив cors

--- backend/start.go
+++ backend/start.go
@@ -4,6 +4,7 @@ import (
 	db "todo-app/database"
 	"todo-app/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,7 +17,15 @@ func main() {
 	db.InitDB()
 	defer db.CloseDB()
 
+	// Конфигурация CORS
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "ngrok-skip-browser-warning"}
+
+	// Запуск
 	router := gin.Default()
+	router.Use(cors.New(config))
 	routes.Initialize(router)
 	router.Run(serverAddr)
 }
